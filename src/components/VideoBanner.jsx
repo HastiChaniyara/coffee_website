91635113ff15bd51117a0dff729c9eaaef9fa0c6
HastiChaniyara <chaniyarahasti22@gmail.com>
2025-07-17 00:07:52 +0530

    url added

--- src/components/VideoBanner.jsx
+++ src/components/VideoBanner.jsx
@@ -5,7 +5,7 @@ const VideoBanner = () => {
   return (
     <div className="video-banner-container">
       <video className="background-video" autoPlay loop muted playsInline>
-        <source src="././public/image_slider/2909914-uhd_3840_2024_24fps.mp4" type="video/mp4" />
+        <source src="/image_slider/2909914-uhd_3840_2024_24fps.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="video-overlay">
